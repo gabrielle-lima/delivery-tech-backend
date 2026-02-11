69339db21e7feccb46873e84e247e5e0d2f6d43c
gabrielle-lima <gaibou_lima@hotmail.com>
2026-02-11 20:06:35 -0300

    Fix errors on compilation

--- src/main/java/com/deliverytech/delivery/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.deliverytech.delivery.repository;
-
-import com.deliverytech.delivery.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
